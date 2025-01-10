d53e4213ce3926f61ac5d85578d3c49860a9519e
Othmani <Othmeni.tarak@gmail.com>
2025-01-10 23:35:29 +0000

    Ajout des fichiers de configuration et script python

---
+++ commande_python.py
@@ -0,0 +1,21 @@
+from netmiko import ConnectHandler
+
+cisco_router = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.30.138',
+    'username': 'admin',
+    'password': 'admin123',
+    'secret': 'admin123',
+    'port': 22,
+}
+
+with open('fichier_commnds.txt', 'r') as file:
+    commands = file.readlines()
+
+conn = ConnectHandler(**cisco_router)
+conn.enable()
+output = conn.send_config_set(commands)
+print(output)
+conn.disconnect()
+
+print("Configuration terminée.")
