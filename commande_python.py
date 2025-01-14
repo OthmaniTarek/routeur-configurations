77e1d5d0bfce9a4c6bc0cc54103fe7aa6d92f02a
OthmaniTarek <othmeni.tarak@gmail.com>
2025-01-14 03:49:50 +0100

    Update commande_python.py

--- commande_python.py
+++ commande_python.py
@@ -2,18 +2,31 @@ from netmiko import ConnectHandler
 
 cisco_router = {
     'device_type': 'cisco_ios',
-    'host': '192.168.30.138',
+    'host': '192.168.30.138', 
     'username': 'admin',
     'password': 'admin123',
     'secret': 'admin123',
     'port': 22,
 }
 
-with open('fichier_commnds.txt', 'r') as file:
-    commands = file.readlines()
-
 conn = ConnectHandler(**cisco_router)
-conn.enable()
-output = conn.send_config_set(commands)
-print(output)
 
+output = conn.send_command('show ip int br')
+with open('interfaces.txt', 'w') as fichier:
+    fichier.write(output)
+
+commandes = [
+    'interface Loopback111',
+    'ip address 10.111.111.111 255.255.255.0',
+    'no shutdown'
+]
+
+conn.enable()
+resultat_config = conn.send_config_set(commandes)
+
+with open('resultat_config.txt', 'w') as fichier:
+    fichier.write(resultat_config)
+
+conn.disconnect()
+
+print('Configuration terminÃ©e.')
